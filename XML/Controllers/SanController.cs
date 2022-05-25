c0a3d8721ed12d4d1df558e879d7a61e69c87057
ngocy007 <dev.yngoc@gmail.com>
2022-05-25 23:52:58 +0700

    full CRUD

--- XML/Controllers/SanController.cs
+++ XML/Controllers/SanController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml;
 using XML.Models;
+using System.Runtime;
 
 namespace XML.Controllers
 {
@@ -72,65 +74,148 @@ namespace XML.Controllers
             san.AppendChild(MASAN);
 
             XmlElement TENSAN = doc.CreateElement("TENSAN");
-            TENSAN.InnerText = sanMoi.MASAN;
+            TENSAN.InnerText = sanMoi.TENSAN;
             san.AppendChild(TENSAN);
 
             XmlElement DIACHI = doc.CreateElement("DIACHI");
-            DIACHI.InnerText = sanMoi.MASAN;
+            DIACHI.InnerText = sanMoi.DIACHI;
             san.AppendChild(DIACHI);
 
             XmlNode temp = root.SelectSingleNode("SANVD[last()]");
 
 
             root.InsertAfter(san, temp);
-            doc.Save(file);
+            doc.Save(File1);
             ViewBag.t = "thanh cong";
-            return View();
+
+
+            XmlNodeList tempSan = root.SelectNodes("SANVD");
+            List<SAN> tempSans = new List<SAN>();
+
+            foreach (XmlNode SanNode in tempSan)
+            {
+                SAN temp1 = new SAN();
+                temp1.MASAN = SanNode["MASAN"].InnerText;
+                temp1.TENSAN = SanNode["TENSAN"].InnerText;
+                temp1.DIACHI = SanNode["DIACHI"].InnerText;
+                tempSans.Add(temp1);
+            }
+
+            return View("Index", tempSans);
+
 
         }
 
         // GET: San/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(string id)
         {
-            return View();
+            initValue();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            try
+            {
+                XmlNode sanCu = root.SelectSingleNode("SANVD[MASAN='" + id + "']");
+
+                SAN temp = new SAN();
+                temp.MASAN = sanCu["MASAN"].InnerText;
+                temp.TENSAN = sanCu["TENSAN"].InnerText;
+                temp.DIACHI = sanCu["DIACHI"].InnerText;
+
+                return View(temp);
+            }
+            catch (Exception)
+            {
+
+                return HttpNotFound();
+            }
         }
 
         // POST: San/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(string id, SAN sanSua)
         {
-            try
-            {
-                // TODO: Add update logic here
+            initValue();
 
-                return RedirectToAction("Index");
-            }
-            catch
+            XmlNode sanCu = root.SelectSingleNode("SANVD[MASAN='" + sanSua.MASAN + "']");
+
+            XmlElement san = doc.CreateElement("SANVD");
+
+            XmlElement MASAN = doc.CreateElement("MASAN");
+            MASAN.InnerText = sanSua.MASAN;
+            san.AppendChild(MASAN);
+
+            XmlElement TENSAN = doc.CreateElement("TENSAN");
+            TENSAN.InnerText = sanSua.TENSAN;
+            san.AppendChild(TENSAN);
+
+            XmlElement DIACHI = doc.CreateElement("DIACHI");
+            DIACHI.InnerText = sanSua.DIACHI;
+            san.AppendChild(DIACHI);
+
+
+
+            root.ReplaceChild(san, sanCu);
+            doc.Save(File1);
+
+
+            XmlNodeList San = root.SelectNodes("SANVD");
+            List<SAN> sans = new List<SAN>();
+
+            foreach (XmlNode SanNode in San)
             {
-                return View();
+                SAN temp = new SAN();
+                temp.MASAN = SanNode["MASAN"].InnerText;
+                temp.TENSAN = SanNode["TENSAN"].InnerText;
+                temp.DIACHI = SanNode["DIACHI"].InnerText;
+                sans.Add(temp);
             }
+
+            return View("Index", sans);
         }
 
         // GET: San/Delete/5
-        public ActionResult Delete(int id)
+
+        [HttpGet]
+        public ActionResult Delete(string id)
         {
+            initValue();
+
+            try
+            {
+                XmlNode sanCu = root.SelectSingleNode("SANVD[MASAN='" + id + "']");
+
+                if (sanCu != null)
+                {
+                    root.RemoveChild(sanCu);
+                    doc.Save(File1);
+
+                    XmlNodeList San = root.SelectNodes("SANVD");
+                    List<SAN> sans = new List<SAN>();
+
+                    foreach (XmlNode SanNode in San)
+                    {
+                        SAN temp = new SAN();
+                        temp.MASAN = SanNode["MASAN"].InnerText;
+                        temp.TENSAN = SanNode["TENSAN"].InnerText;
+                        temp.DIACHI = SanNode["DIACHI"].InnerText;
+                        sans.Add(temp);
+                    }
+
+                    return View("Index", sans);
+                }
+
+            }
+            catch (Exception)
+            {
+
+                return HttpNotFound();
+            }
+
+
             return View();
         }
 
-        // POST: San/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
